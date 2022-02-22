10fcf3c5fac6fe8de8aa205f66c876942a7d2763
Mihai Potra <mike@mpotra.com>
2022-02-23 01:40:49 +0200

    Prepare hex package

--- mix.exs
+++ mix.exs
@@ -1,13 +1,17 @@
 defmodule Samlo.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/mpotra/samlo"
+
   def project do
     [
       app: :samlo,
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: "Elixir SAML library and structures",
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -23,6 +27,16 @@ defmodule Samlo.MixProject do
     [
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  def package do
+    [
+      name: :samlo,
+      files: ~w(lib LICENSE mix.exs README.md),
+      maintainers: ["Mihai Potra"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
     ]
   end
 end
